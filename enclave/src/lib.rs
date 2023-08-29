13809a6d9039004ea4ee08dd5c78c7dd80531715
Guozhang He <jxgzh@qq.com>
2023-08-29 16:38:41 +0800

    ✨ feat: Add generate quote function
    Adjusted the output of some error messages
    
    Signed-off-by: Guozhang He <jxgzh@qq.com>

--- enclave/src/lib.rs
+++ enclave/src/lib.rs
@@ -79,23 +79,25 @@ struct RSAKeyData {
 const RSA_DURATION: u64 = 330;
 const ADVANCE_REFRESH_TIME: i64 = 300;
 
+const RSA_KEY_PATH: &str = "RSA_KEY";
+
 lazy_static! {
     static ref RSA_KEY_DATA: SgxMutex<Option<RSAKeyData>> = SgxMutex::new(None);
 }
-// extern "C" {
-//     pub fn ocall_get_target_info(
-//         ret_val  : *mut sgx_status_t,
-//         ti       : *mut sgx_target_info_t
-//     ) -> sgx_status_t;
+extern "C" {
+    pub fn ocall_get_target_info(
+        ret_val  : *mut sgx_status_t,
+        ti       : *mut sgx_target_info_t
+    ) -> sgx_status_t;
 
-//     pub fn ocall_generate_quote(
-//         ret_val        : *mut sgx_status_t,
-//         p_report       : *mut sgx_report_t,
-//         p_quote        : *mut u8,
-//         max_quote_len  : u32,
-//         p_quote_len    : *mut u32
-//     ) -> sgx_status_t;
-// }
+    pub fn ocall_generate_quote(
+        ret_val        : *mut sgx_status_t,
+        p_report       : *mut sgx_report_t,
+        p_quote        : *mut u8,
+        max_quote_len  : u32,
+        p_quote_len    : *mut u32
+    ) -> sgx_status_t;
+}
 
 fn write_rsa_key(path: &str) -> Result<(), sgx_status_t> {
     let helper = SerializeHelper::new();
@@ -104,7 +106,7 @@ fn write_rsa_key(path: &str) -> Result<(), sgx_status_t> {
     let rsa_key_bytes = match helper.encode(rsa_key_data) {
         Some(d) => d,
         None => {
-            println!("Encode data failed.");
+            println!("[-] Encode data failed.");
             return Err(sgx_status_t::SGX_ERROR_UNEXPECTED);
         },
     };
@@ -112,7 +114,7 @@ fn write_rsa_key(path: &str) -> Result<(), sgx_status_t> {
     let mut file = match SgxFile::create(path) {
         Ok(f) => f,
         Err(_) => {
-            println!("SgxFile::create failed.");
+            println!("[-] SgxFile::create failed.");
             return Err(sgx_status_t::SGX_ERROR_FILE_CANT_OPEN_RECOVERY_FILE);
         },
     };
@@ -120,12 +122,12 @@ fn write_rsa_key(path: &str) -> Result<(), sgx_status_t> {
     let write_size = match file.write(rsa_key_bytes.as_slice()) {
         Ok(len) => len,
         Err(_) => {
-            println!("SgxFile::write failed.");
+            println!("[-] SgxFile::write failed.");
             return Err(sgx_status_t::SGX_ERROR_FILE_CANT_OPEN_RECOVERY_FILE);
         },
     };
 
-    println!("File written successfully, write size: {}.", write_size);
+    println!("[+] File written successfully, write size: {}.", write_size);
     Ok(())
 }
 
@@ -133,7 +135,7 @@ fn read_rsa_key(path: &str) -> Result<(), sgx_status_t> {
     let mut file = match SgxFile::open(path) {
         Ok(f) => f,
         Err(_) => {
-            println!("SgxFile::open failed.");
+            println!("[-] SgxFile::open failed.");
             return Err(sgx_status_t::SGX_ERROR_FILE_CANT_OPEN_RECOVERY_FILE);
         },
     };
@@ -143,13 +145,13 @@ fn read_rsa_key(path: &str) -> Result<(), sgx_status_t> {
     let read_size = match file.read_to_end(&mut data) {
         Ok(len) => len,
         Err(_) => {
-            println!("SgxFile::read failed.");
+            println!("[-] SgxFile::read failed.");
             return Err(sgx_status_t::SGX_ERROR_FILE_BAD_STATUS);
         },
     };
 
     if read_size == 0 {
-        println!("{} file is empty.", path);
+        println!("[-] {} file is empty.", path);
         return Err(sgx_status_t::SGX_ERROR_UNEXPECTED);
     }
 
@@ -157,7 +159,7 @@ fn read_rsa_key(path: &str) -> Result<(), sgx_status_t> {
     let rsa_key_data = match helper.decode() {
         Some(d) => d,
         None => {
-            println!("Decode data failed.");
+            println!("[-] Decode data failed.");
             return Err(sgx_status_t::SGX_ERROR_UNEXPECTED);
         },
     };
@@ -165,14 +167,14 @@ fn read_rsa_key(path: &str) -> Result<(), sgx_status_t> {
     let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
     // 5 minutes before the deadline still needs to update the key
     if (rsa_key_data.not_befer > now as i64) | (rsa_key_data.not_after - ADVANCE_REFRESH_TIME < now as i64) {
-        println!("Rsa key not in time range.");
+        println!("[-] Rsa key not in time range.");
         return Err(sgx_status_t::SGX_ERROR_UNEXPECTED);
     }
 
     let mut rsa_data = RSA_KEY_DATA.lock().unwrap();
     *rsa_data = Some(rsa_key_data);
 
-    println!("File read successfully, read size: {}.", read_size);
+    println!("[+] File read successfully, read size: {}.", read_size);
     Ok(())
 }
 
@@ -210,27 +212,123 @@ fn update_rsa_key(path: &str) -> Result<(), sgx_status_t> {
                     not_after: (now + RSA_DURATION) as time_t,
                 });
     }
+    println!("[+] Rsa key updated successfully!");
 
     write_rsa_key(path)?;
-
-    println!("Rsa key updated successfully !");
     Ok(())
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn init_rsa_key() -> sgx_status_t {
-    let rsa_key_path = "RSA_KEY";
-
-    match read_rsa_key(rsa_key_path) {
+fn init_rsa_key() -> Result<(), sgx_status_t> {
+    match read_rsa_key(RSA_KEY_PATH) {
         Ok(_) => (),
         Err(_) => {
-            match update_rsa_key(rsa_key_path) {
-                Ok(_) => (),
-                Err(x) => return x
-            };
+            println!("[+] Trying to generate a new RSA key.");
+            update_rsa_key(RSA_KEY_PATH)?;
         }
     };
 
-    println!("[ENCLAVE] init_rsa_key() success!");
+    println!("[+] init_rsa_key() success!");
+    Ok(())
+}
+
+fn generate_quote() -> Option<Vec<u8>>{
+    let mut rsa_key_data: RSAKeyData = (*RSA_KEY_DATA.lock().unwrap()).clone().unwrap();
+
+    let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+    // 5 minutes before the deadline still needs to update the key
+    if (rsa_key_data.not_befer < now as i64) | (rsa_key_data.not_after - ADVANCE_REFRESH_TIME < now as i64) {
+        println!("[ENCLAVE] Rsa key not in time range. Generate new RSA key as quote customization data");
+        match update_rsa_key(RSA_KEY_PATH) {
+            Ok(_) => {
+                rsa_key_data = (*RSA_KEY_DATA.lock().unwrap()).clone().unwrap()
+            },
+            Err(_) => return None
+        };
+    }
+
+    let rsa3072_public_key = sgx_rsa3072_public_key_t {
+        modulus: rsa_key_data.modulus.clone().try_into().unwrap(),
+        exponent: rsa_key_data.e.clone().try_into().unwrap(),
+    };
+
+    let mut rt : sgx_status_t = sgx_status_t::SGX_ERROR_UNEXPECTED;
+    let mut ti : sgx_target_info_t = sgx_target_info_t::default();
+
+    let result = unsafe {
+        ocall_get_target_info(
+            &mut rt as *mut sgx_status_t,
+            &mut ti as *mut _)
+    };
+
+    if result != sgx_status_t::SGX_SUCCESS {
+        return None;
+    }
+
+    let mut public_key: Vec<u8> = Vec::new();
+    public_key.extend(&rsa3072_public_key.modulus);
+    public_key.extend(&rsa3072_public_key.exponent);
+
+    let hash_public_key = rsgx_sha256_slice(public_key.as_slice()).unwrap();
+    let mut data = [0; 64];
+    data[..hash_public_key.len()].copy_from_slice(&hash_public_key);
+    println!("[+] get Hash(rsa public key) for report");
+    let report_data: sgx_report_data_t = sgx_report_data_t {
+        d: data
+    };
+    let mut report = match rsgx_create_report(&ti, &report_data) {
+        Ok(report) => report,
+        Err(x) => {
+            println!("[-] rsgx_create_report() failed, error code: {:?}", x);
+            return None;
+        }
+    };
+
+    const RET_QUOTE_BUF_LEN : u32 = 9999;
+    let mut return_quote_buf : [u8; RET_QUOTE_BUF_LEN as usize] = [0;RET_QUOTE_BUF_LEN as usize];
+    let mut quote_len : u32 = 0;
+
+    let p_report = &mut report as *mut sgx_report_t;
+    let p_quote = return_quote_buf.as_mut_ptr();
+    let max_quote_len = RET_QUOTE_BUF_LEN;
+    let p_quote_len = &mut quote_len as *mut u32;
+
+    let result = unsafe {
+        ocall_generate_quote(
+            &mut rt as *mut sgx_status_t,
+            p_report,
+            p_quote, 
+            max_quote_len,
+            p_quote_len)
+    };
+    match result {
+        sgx_status_t::SGX_SUCCESS => (),
+        _ => return None,
+    };
+
+    Some(return_quote_buf[0..quote_len as usize].to_vec())
+
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn run_server() -> sgx_status_t {
+    match init_rsa_key() {
+        Ok(_) => (),
+        Err(x) => {
+            println!("[-] RSA key initialization failure.");
+            return x
+        }
+    };
+
+    let quote = match generate_quote() {
+        Some(x) => {
+            println!("[+] Generate Quote successfully!");
+            x
+        },
+        None => {
+            println!("[-] Failed to generate quote");
+            return sgx_status_t::SGX_ERROR_UNEXPECTED;
+        }
+    };
+
     sgx_status_t::SGX_SUCCESS
 }

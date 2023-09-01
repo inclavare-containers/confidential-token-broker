24e49485fc8f32121c5c8a14e45c548aa7d62bd6
Guozhang He <jxgzh@qq.com>
2023-09-01 14:15:15 +0800

    ✨ feat: Add logging
    Signed-off-by: Guozhang He <jxgzh@qq.com>

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,8 +1,12 @@
 extern crate sgx_types;
 extern crate sgx_urts;
+extern crate log;
+extern crate env_logger;
 
 use sgx_types::*;
 use sgx_urts::SgxEnclave;
+use log::{info, warn, error, LevelFilter};
+use env_logger::Builder;
 
 static ENCLAVE_FILE: &'static str = "enclave.signed.so";
 
@@ -10,8 +14,8 @@ extern {
     fn run_server(eid: sgx_enclave_id_t, retval: *mut sgx_status_t) -> sgx_status_t;
 }
 
-
 fn init_enclave() -> SgxResult<SgxEnclave> {
+    info!("Call init_enclave()");
     let mut launch_token: sgx_launch_token_t = [0; 1024];
     let mut launch_token_updated: i32 = 0;
     // call sgx_create_enclave to initialize an enclave instance
@@ -26,55 +30,48 @@ fn init_enclave() -> SgxResult<SgxEnclave> {
 }
 
 #[no_mangle]
-pub extern "C"
-fn ocall_get_target_info(
+pub extern "C" fn ocall_get_target_info(
     ti: *mut sgx_target_info_t
 ) -> sgx_status_t {
-    println!("[APP] Call ocall_get_target_info()");
     let result = unsafe {
         sgx_qe_get_target_info(ti)
     };
     match result {
         sgx_quote3_error_t::SGX_QL_SUCCESS => return sgx_status_t::SGX_SUCCESS,
         other => {
-            println!("[APP] error in ocall_get_target_info(): {:?}", other);
+            error!("Error in ocall_get_target_info(): {:?}", other);
             return sgx_status_t::SGX_ERROR_UNEXPECTED;
         }
     }
 }
 
 #[no_mangle]
-pub extern "C"
-fn ocall_generate_quote(
+pub extern "C" fn ocall_generate_quote(
     p_report           : *mut sgx_report_t,
     p_quote            : *mut u8,
     max_quote_len      : u32,
     p_quote_len        : *mut u32
 ) -> sgx_status_t {
-    println!("[APP] Call ocall_generate_quote()");
-    println!("[APP] Call sgx_qe_get_quote_size()");
-
     let mut quote_size: u32 = 0;
 
     let qe3_ret = unsafe { sgx_qe_get_quote_size(&mut quote_size as _) };
     if qe3_ret != sgx_quote3_error_t::SGX_QL_SUCCESS {
-        println!("[-] Error in sgx_qe_get_quote_size(), error code: {:?}", qe3_ret);
+        error!("Error in sgx_qe_get_quote_size(), error code: {:?}", qe3_ret);
         return sgx_status_t::SGX_ERROR_UNEXPECTED;
     }
 
     if quote_size > max_quote_len {
-        println!("[-] The quote size({:?}) exceeds the maximum value {:?}", quote_size, max_quote_len);
+        error!("The quote size({:?}) exceeds the maximum value {:?}", quote_size, max_quote_len);
         return sgx_status_t::SGX_ERROR_UNEXPECTED;
     }
 
     let mut quote_vec: Vec<u8> = vec![0; quote_size as usize];
 
-    println!("[APP] Call sgx_qe_get_quote()");
     let qe3_ret =
         unsafe { sgx_qe_get_quote(p_report as _, quote_size, quote_vec.as_mut_ptr() as _) };
 
     if qe3_ret != sgx_quote3_error_t::SGX_QL_SUCCESS {
-        println!("[-] Error in sgx_qe_get_quote(), error code: {:?}", qe3_ret);
+        error!("Error in sgx_qe_get_quote(), error code: {:?}", qe3_ret);
         return sgx_status_t::SGX_ERROR_UNEXPECTED;
     }
 
@@ -90,13 +87,17 @@ fn ocall_generate_quote(
 }
 
 fn main() {
+    Builder::new()
+        .filter(None, LevelFilter::Info)
+        .init();
+
     let enclave = match init_enclave() {
         Ok(r) => {
-            println!("[+] Init Enclave Successful {}!", r.geteid());
+            info!("Init Enclave Successful! Enclave id: {}", r.geteid());
             r
         },
         Err(x) => {
-            println!("[-] Init Enclave Failed {}!", x.as_str());
+            error!("Init Enclave Failed {}!", x.as_str());
             return;
         },
     };
@@ -110,7 +111,7 @@ fn main() {
     match result {
         sgx_status_t::SGX_SUCCESS => {},
         _ => {
-            println!("[-] ECALL Enclave Failed {}!", result.as_str());
+            error!("ECALL Enclave Failed {}!", result.as_str());
             return;
         }
     }
